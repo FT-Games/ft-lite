cdbe5b1391165bae89713f01b0bd83115df471d6
TyCo <tybridges44@hotmail.com>
2026-07-14 06:24:47 +0000

    add fix to suggestion box and preview beta

---
+++ .dev/preview.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clear
+
+echo "██████╗ ██████╗ ███████╗██╗   ██╗██╗███████╗██╗    ██╗"
+echo "██╔══██╗██╔══██╗██╔════╝██║   ██║██║██╔════╝██║    ██║"
+echo "██████╔╝██████╔╝█████╗  ██║   ██║██║█████╗  ██║ █╗ ██║"
+echo "██╔═══╝ ██╔══██╗██╔══╝  ╚██╗ ██╔╝██║██╔══╝  ██║███╗██║"
+echo "██║     ██║  ██║███████╗ ╚████╔╝ ██║███████╗╚███╔███╔╝"
+echo "╚═╝     ╚═╝  ╚═╝╚══════╝  ╚═══╝  ╚═╝╚══════╝ ╚══╝╚══╝"
+echo ""
+echo "Welcome to Preview by FT Games!"
+echo ""
+
+echo "Enter port for preview to be hosted on (e.g. 8080):"
+read port
+
+echo ""
+echo "Server will start on port $port."
+echo "You should see a notification in your code editor."
+echo "Press Ctrl+C to stop the server."
+echo ""
+
+npx http-server -p "$port"
